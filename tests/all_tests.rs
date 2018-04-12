fcdf2d453e971e0a5189b78296a57affaf159795
Emad Jacob Maroun <emoun.open@gmail.com>
2018-04-13 00:22:20 +0200

    Protoyping recursive macros

--- tests/all_tests.rs
+++ tests/all_tests.rs
@@ -8,4 +8,77 @@ fn test(){
 }
 
 
+use std::marker::PhantomData;
+
+macro_rules! dec_struct{
+	{
+		[$name:ident]
+		[$($generics:tt)*]
+		[$($constraints:tt)*]
+		[$($body:tt)*]
+	}=>{
+		struct $name<$($generics)*>
+		where $($constraints)*
+		{$($body)*}
+	};
+}
+
+macro_rules! recursive{
+	{
+		[$($prefix:tt)*]
+		[$($postfix:tt)*]
+	}=>{
+		$($prefix)* $($postfix)*
+	};
+	{
+		[$($prefix:tt)*]
+		[$($postfix:tt)*]
+		$($rest:tt)+
+	}=>{
+		recursive!{[$($prefix)* $($postfix)*] $($rest)*}
+	};
+    {
+		[$($prefix:tt)*]
+		$m:ident!{$($argument:tt)*}
+		$($rest:tt)*
+    }=>{
+        $m!{@recursive
+        	[[$($prefix)*] $($rest)*]
+			$($argument)*
+		}
+    };
+}
+
+macro_rules! mac1{
+    
+    {
+        @recursive [[$($prefix:tt)*] $($postfix:tt)*]
+        $typ:tt
+    }=>{
+        recursive!{[$($prefix)* $typ] $($postfix)*}
+    };
+}
+macro_rules! mac2{
+    ($V:ident,$eq:tt)=>{
+        recursive!{
+            [struct $V<V,W> where W:]
+            mac1!{$eq}
+            [, V:]
+            mac1!{$eq}
+            [{ph1: PhantomData<W>,ph2: PhantomData<V>}]
+        }
+    }
+}
+
+trace_macros!{true}
+mac2!(SomeStruct, PartialEq);
+/* We want it to expand to:
+
+	struct SomeStruct < V , W >
+		where W : PartialEq , V : PartialEq
+	{
+		ph1 : PhantomData < W > ,
+		ph2 : PhantomData < V >
+	}
+*/
 
